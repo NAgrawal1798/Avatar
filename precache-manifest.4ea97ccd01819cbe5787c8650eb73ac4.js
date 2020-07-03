a44269e499693907f98ef07fa12e937fbb9ccdae
NAgrawal1798 <agrawal.nikhil1798@gmail.com>
2020-07-03 10:32:39 +0530

    Updates

---
+++ precache-manifest.4ea97ccd01819cbe5787c8650eb73ac4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedc6a3bc1fd362e3c10be5fb0ed4021",
+    "url": "/Avatar/index.html"
+  },
+  {
+    "revision": "137e412718e5f664ab18",
+    "url": "/Avatar/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e8dd61b1198aa9493ee",
+    "url": "/Avatar/static/css/main.20de9f2c.chunk.css"
+  },
+  {
+    "revision": "137e412718e5f664ab18",
+    "url": "/Avatar/static/js/2.7e4a7f03.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Avatar/static/js/2.7e4a7f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8dd61b1198aa9493ee",
+    "url": "/Avatar/static/js/main.d22c963e.chunk.js"
+  },
+  {
+    "revision": "e27eda63319aa5bae6c5",
+    "url": "/Avatar/static/js/runtime-main.d3ec4c6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Avatar/static/media/logo.5d5d9eef.svg"
+  }
+]);
